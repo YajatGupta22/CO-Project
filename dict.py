628f705ecdb0d6095c21deff906b775b57f9ced2
just-a-penguin <105591284+just-a-penguin@users.noreply.github.com>
2022-06-24 14:56:42 +0530

    Create dict.py

---
+++ dict.py
@@ -0,0 +1,5 @@
+dict0 = {"add":10000,"sub":10001,"mov1":10010,"mov2":10011,"ld":10100,"st":10101,"mul":10110
+         "div":10111,"rs":11000,"ls":11001,"xor":11010,"or":11011,"and":11100,"not":11101,
+         "cmp":11110,"jmp":11111,"jlt":"01110","jgt" :"01101","je":"01111","hlt":"01010"
+         }
+# basic dictionary for code and we cant take 01011 things as int in dict so had to take them as strings.
