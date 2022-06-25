16415f5b0afa61f739b46f9040d7a04a72a42346
just-a-penguin <105591284+just-a-penguin@users.noreply.github.com>
2022-06-25 22:05:36 +0530

    Update A_basic_coversion_algo.py

--- A_basic_coversion_algo.py
+++ A_basic_coversion_algo.py
@@ -20,7 +20,7 @@ def decimalToBinary2(n):
 dict0 = {"add": "10000", "sub": "10001", "mov1": "10010", "mov2": "10011", "ld": "10100", "st": "10101", "mul": "10110",
          "div": "10111", "rs": "11000", "ls": "11001", "xor": "11010", "or": "11011", "and": "11100", "not": "11101",
          "cmp": "11110", "jmp": "11111", "jlt": "01110", "jgt": "01101", "je": "01111", "hlt": "01010",
-         "mem1": 1
+         "mem1": 3
          }
 reg = {"R0": "000", "R1": "001", "R2": "010", "R3": "011", "R4": "100", "R5": "101", "R6": "110", "FLAGS": "111"
        }
@@ -35,10 +35,11 @@ op5 = ["ld", "st"]
 
 def convert(sen):
     sen_list = [x for x in sen.split()]
+    assert sen_list[0] in dict0, "Syntax Error! Operator not provided in ISO"
     if sen_list[0] != "mov":
         sen_list_assem = [dict0[sen_list[0]]]
     else:
-        if (sen_list[2] in reg):
+        if sen_list[2] in reg:
             sen_list_assem = [dict0["mov2"]]
         else:
             sen_list_assem = [dict0["mov1"]]
@@ -51,26 +52,23 @@ def convert(sen):
         for i in range(2):
             sen_list_assem.append(reg[sen_list[i + 1]])
     elif sen_list[0] in op3:
-        sen_list_assem.append(decimalToBinary2(mem_add))
-        mem_add = mem_add + 1
-
+        sen_list_assem.append(variables[sen_list[1]])
     elif sen_list[0] in op4:
         sen_list_assem.append(reg[sen_list[1]])
         sen_list_assem.append(decimalToBinary(sen_list[2]))
+    elif sen_list[0] in op5:
+        sen_list_assem.append(reg[sen_list[1]])
+        sen_list_assem.append(variables[sen_list[2]])
     elif sen_list[0] == "hlt":
         sen_list_assem.append("00000000000")
     elif sen_list[0] == "mov":
         if sen_list[2] not in reg:
             sen_list_assem.append(reg[sen_list[1]])
             sen_list_assem.append(decimalToBinary(sen_list[2]))
-
         else:
             sen_list_assem.append("00000")
             sen_list_assem.append(reg[sen_list[1]])
             sen_list_assem.append(reg[sen_list[2]])
-    elif sen_list[0] in op5:
-        sen_list_assem.append("000")
-
     print(*sen_list_assem, sep="")
 
 
@@ -83,8 +81,6 @@ for i in range(m):
         convert(sen)
     else:
         sen_list = sen.split()
-
         if sen_list[1] not in variables:
             variables[sen_list[1]] = decimalToBinary2(dict0["mem1"])
             dict0["mem1"] += 1
-
