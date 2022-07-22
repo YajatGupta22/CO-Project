a5c242e2b45446f094cd23a75f51c9d2ad18376d
Sanskar Ranjan <105591284+just-a-penguin@users.noreply.github.com>
2022-07-22 12:32:31 +0530

    Update simulator_01.py

--- simulator_01.py
+++ simulator_01.py
@@ -8,14 +8,27 @@ def decimalToBinary(n):
     bnr = x[::-1]
     return bnr
 
+def binaryToDecimal(binary):
+    binary=int(binary)
+    decimal=0
+    i = 0
+    while(binary != 0):
+        dec = binary % 10
+        decimal+=dec * pow(2, i)
+        binary = binary//10
+        i += 1
+    return decimal 
+    
+
+
 op1_rev=['10000', '10001', '10110', '11010', '11011', '11100']
 op2_rev=['10111', '11101', '11110']
 op3_rev=['11111', '01100', '01101', '01111']
 op4_rev=['11000', '11001']
 op5_rev=['10100', '10101']
 
-mem_adds=[00000000]*256
-pC=00000000
+mem_adds=["00000000"]*256
+pC=0
 reg_val={'000':0, '001':0, '010':0, '011':0, '100':0, '101':0, '110':0, "111":0}
 inputs=[]
 
@@ -37,7 +50,14 @@ def execute(sen):
     if op=="10110":         #   Multiply
         reg_val[sen[7:9]]=reg_val[sen[10:12]]*reg_val[sen[13:15]]
     if op=="10111":         #   Divide    
-        reg_val[sen[7:9]]=reg_val[sen[10:12]]/reg_val[sen[13:15]]
-    if op=="10011":         #   Mov
+        reg_val[sen[7:9]]=int(reg_val[sen[10:12]]/reg_val[sen[13:15]])
+    if op=="10011":         #   Mov reg
         reg_val[sen[10:12]]=reg_val[sen[13:15]]
-    if op=="1001"
+    if op=="10010":         #   Mov imm
+        reg_val[sen[5:7]]=decimalToBinary(sen[8:15])
+    if op=="10100":         #   Load
+
+
+
+
+
